1f42f9ae168e9c2db4faa5860e65eca79eb1beff
cstensonpv <cstenson@kth.se>
2015-03-14 09:43:57 +0100

    cstensonpv  Första commit har enbart försökt få in de flesta filer vi behöver.

---
+++ app/js/quizModel.js
@@ -0,0 +1,10 @@
+// Here we create an Angular service that we will use for our 
+// model. In your controllers (or other services) you can include the
+// dependency on any service you need. Angular will insure that the
+// service is created first time it is needed and then just reuse it
+// the next time.
+quizApp.factory('quizModel',function ($resource, $cookieStore) { 
+  
+  
+
+});
---
+++ app/js/testCtrl.js
@@ -0,0 +1,10 @@
+// Dinner controller that we use whenever we have view that needs to 
+// display or modify the dinner menu
+quizApp.controller('testCtrl', function ($scope,quizModel,$cookieStore) {
+  
+
+
+  // TODO in Lab 5: Implement the methods to get the dinner menu
+  // add dish to menu and get total menu price
+
+});
