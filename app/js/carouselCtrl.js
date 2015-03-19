20673888c518c9c7b245e4e06f24dce61a92f910
Emil Westin <emiwes@csc.kth.se>
2015-03-19 12:37:48 +0100

    Emil, Calle ändrat Karusellen och css

--- app/js/carouselCtrl.js
+++ app/js/carouselCtrl.js
@@ -1,33 +1,16 @@
-quizApp.controller('CarouselCtrl', function($scope) {
+quizApp.controller('CarouselCtrl', function($scope,quizModel) {
   
   // '-10' due to angular restrictions to pausing the carousel
   $scope.myInterval = -10;
+  $scope.slides = [];
+  var slide = [];
+  var quiz = quizModel.getQuiz();
 
-  // connect to quizObject in model
-  $scope.slides = [
-    {image: 'http://www.smashingmagazine.com/images/music-cd-covers/27.jpg'},
-    {image: 'http://www.mp3jam.org/blog/wp-content/uploads/2014/05/d-face-for-christina-aguilera-bionic-album-cover.jpg'},
-    {image: 'http://truenorthinc.com/wp-content/uploads/2013/05/album_art_09.jpg'},
-    {image: 'http://editorial.designtaxi.com/news-super0703/4.jpg'},
-    {image: 'http://www.smashingmagazine.com/images/music-cd-covers/27.jpg'},
-    {image: 'http://www.mp3jam.org/blog/wp-content/uploads/2014/05/d-face-for-christina-aguilera-bionic-album-cover.jpg'},
-    {image: 'http://truenorthinc.com/wp-content/uploads/2013/05/album_art_09.jpg'}
-  ];
-  
-  $scope.$watch('slides', function(values) {
-
-    var allSlides = [];
-    var oneSlide = [];
-
-    for (var i = 0; i < $scope.slides.length; i ++) {
-      oneSlide.push($scope.slides[i]); 
-
-      if ((i+1)%6 === 0 || i === $scope.slides.length-1) {
-        allSlides.push(oneSlide);
-        oneSlide=[];
-      }
+  for(var k = 0; k<quiz.questions.length; k++){
+    slide.push(quiz.questions[k]);
+    if((k+1)%6 === 0 || (k+1) === quiz.questions.length){
+      $scope.slides.push(slide);
+      slide = [];
     }
-
-    $scope.groupedSlides = allSlides;
-  }, true);
+  }
 });
